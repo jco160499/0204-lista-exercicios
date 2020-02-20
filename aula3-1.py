74b4f476078e25e3bfe204be5b1c44ef13023e86
João <jcoliveira160499@gmail.com>
2020-02-20 14:49:51 -0300

    Exercício 1 - aula 3

---
+++ aula3-1.py
@@ -0,0 +1,12 @@
+import datetime as dt
+def dias_de_vida(ano,mes,dia):
+    data_nascimento = dt.date(ano,mes,dia)
+    idade_dias =  dt.date.today() - data_nascimento
+    return idade_dias
+
+print(dias_de_vida(1999,4,16))
+idade = 7615/365
+dias = str(dias_de_vida(1999,4,16)).replace(' days, 0:00:00','')
+idade2 = round(float(dias)/365,1)
+print('Você tem:    ',idade2, ' anos')
+
