157c654e42323baa88d50e839d67867289578005
João <jcoliveira160499@gmail.com>
2020-02-20 12:41:57 -0300

    Procedimentos usar livrarias

---
+++ teste-livrarias.py
@@ -0,0 +1,10 @@
+#python3 -m venv env
+#source env/bin/activate
+#pip install datetime
+#pip install pandas
+#pip install numpy
+#PRECISA ACTIVATE PARA IMPORTAR AS LIVRARIAS
+import numpy as np
+a = np.array([1,2,3])
+b = np.array([4,5,6])
+print (a/b) 
